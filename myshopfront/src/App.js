96a8224c2cfa9c061c28ca3e76a5ac25d4ac497d
TANK <gholizadeh.pro@gmail.com>
2022-07-09 01:11:57 +0430

    some css files and codes removed.

--- myshopfront/src/App.js
+++ myshopfront/src/App.js
@@ -1,19 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
 import { Routes, Route } from "react-router-dom"
 import CategoryIndex from './components/category/CategoryIndex';
 import { CategoryProvider } from './contexts/CategoryContext';
 function App() {
   return (
+    <>
     <CategoryProvider>
-      <div className='app'>
         <Routes>
           <Route></Route>
           <Route path='categories' element={ <CategoryIndex /> }></Route>
         </Routes>
-      </div>
     </CategoryProvider>
-   
+    </>
   );
 }
 
