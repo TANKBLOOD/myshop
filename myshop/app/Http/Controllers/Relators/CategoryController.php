0ccf1b9bf5b7ab3546fce0fdaf1fcf39e553d100
TANK <gholizadeh.pro@gmail.com>
2022-07-14 00:25:57 +0430

    create category backend initialized.

--- myshop/app/Http/Controllers/Relators/CategoryController.php
+++ myshop/app/Http/Controllers/Relators/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Relators;
 
 use App\Http\Controllers\Controller;
 use App\Models\Relators\Category;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -12,6 +13,21 @@ class CategoryController extends Controller
         $categories= Category::all();
         return response()->json([
             'categories'=> $categories,
+        ]);
+    }
+
+    public function create(Request $request) {
+        $newCat= new Category();
+        $newCat->parent_id= $request->catParent;
+        $newCat->title= $request->catTitle;
+        $newCat->meta_title= $request->catMetaDescription;
+        $newCat->content= $request->catDescription;
+        $newCat->slug= Str::slug($request->catTitle);
+        //upload the image here.
+        $newCat->save();
+
+        return response()->json([
+            'status'=> ' we are okay',
         ]);
     }
 }
