1ec81cb718f353011197d0cfff20cd776e1361e5
TANK <gholizadeh.pro@gmail.com>
2022-07-25 23:04:59 +0430

    product  route binding changed to slug.

--- myshop/app/Models/Presentable/Product.php
+++ myshop/app/Models/Presentable/Product.php
@@ -8,6 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    //changing the key from id to slug
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 
     protected $casts = [
         'images' => 'array',
