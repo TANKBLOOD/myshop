7b0e92c2f2289f0cb3555f0ca891d097d3b22125
TANK <gholizadeh.pro@gmail.com>
2022-08-10 13:34:06 +0430

    product and category relation ship defiend.

--- myshop/app/Models/Presentable/Product.php
+++ myshop/app/Models/Presentable/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Presentable;
 
+use App\Models\Relators\Category;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -18,4 +19,8 @@ class Product extends Model
         'images' => 'array',
         'product_specifications'=> 'object',
     ];
+
+    public function category() {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
 }
--- myshop/app/Models/Relators/Category.php
+++ myshop/app/Models/Relators/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Relators;
 
+use App\Models\Presentable\Product;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,13 @@ class Category extends Model
     use HasFactory;
 
     protected $table= "categories";
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function products() {
+        return $this->hasMany(Product::class, 'category_id', 'id');
+    }
 }
