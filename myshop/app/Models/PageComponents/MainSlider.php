f33a615166516d7b2861047264e7da877f482aaf
TANK <gholizadeh.pro@gmail.com>
2022-10-13 19:01:40 +0330

    Create MainSlider.php

---
+++ myshop/app/Models/PageComponents/MainSlider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\PageComponents;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MainSlider extends Model
+{
+    use HasFactory;
+    protected $table= 'main_slider';
+}
