459a8f273f7e463a3a8c6d5ad5aca15d43d49dff
TANK <gholizadeh.pro@gmail.com>
2022-10-13 19:01:51 +0330

    Update api.php

--- myshop/routes/api.php
+++ myshop/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageComponents\MainSliderController;
 use App\Http\Controllers\presentable\ProductController;
 use App\Http\Controllers\Relators\CategoryController;
 use Illuminate\Http\Request;
@@ -19,7 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+//page components
+Route::post('/mainSlider/create', [MainSliderController::class, 'create']);
+//category section
 Route::get('/category/index', [CategoryController::class, 'index']);
 Route::post('/category/create', [CategoryController::class, 'create']);
 Route::post('/category/delete', [CategoryController::class, 'delete']);
@@ -28,17 +31,15 @@ Route::post('/category/makeSpecial', [CategoryController::class, 'makeSpecial'])
 Route::post('/category/makeNormal', [CategoryController::class, 'makeNormal']);
 Route::get('/category/specials',  [CategoryController::class, 'getSpecialCategories']);
 Route::get('/category/view/{category}', [CategoryController::class, 'view']);
+Route::get('/category/{category}/products', [ProductController::class, 'categoryProducts']);
 
+//product Section
 Route::get('/product/index', [ProductController::class, 'index']);
 Route::post('/product/create', [ProductController::class, 'create']);
-
 Route::get('/product/view/{product}', [ProductController::class, 'view']);
 Route::post('/product/update', [ProductController::class, 'update']);
 Route::get('/product/search/{term?}', [ProductController::class , 'search']);
-
 Route::get('/products/popular', [ProductController::class, 'popularList']);
 Route::post('/products/makePopular', [ProductController::class, 'makePopular']);
 Route::post('/products/removePopular', [ProductController::class, 'removePopular']);
 
-
-Route::get('/category/{category}/products', [ProductController::class, 'categoryProducts']);
