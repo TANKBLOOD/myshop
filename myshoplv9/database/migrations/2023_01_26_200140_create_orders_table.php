3af2f8cee3c2f80013400bd4afb5d7934e5417a9
Ali Gholizadeh <71063386+TANKBLOOD@users.noreply.github.com>
2023-01-27 00:54:03 +0330

    order model created.

---
+++ myshoplv9/database/migrations/2023_01_26_200140_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->set('status', ['unpaid', 'pending', 'preparing', 'readyToSend', 'sent', 'received'])->default('unpaid');
+            $table->unsignedBigInteger('send_type_id');
+            $table->unsignedBigInteger('address_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
