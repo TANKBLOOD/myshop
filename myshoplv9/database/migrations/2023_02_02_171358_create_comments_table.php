44267b842b02f680dd30acf373f6858b9ab4ee88
Ali Gholizadeh <71063386+TANKBLOOD@users.noreply.github.com>
2023-02-02 21:45:19 +0330

    comment model created.

---
+++ myshoplv9/database/migrations/2023_02_02_171358_create_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('parent_id');
+            $table->text('content');
+            $table->integer('rating')->nullable();
+            $table->set('status', ['pending', 'approved', 'published', 'rejected'])->default('pending');
+            $table->string('email')->nullable();
+            $table->string('name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
