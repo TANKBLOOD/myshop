98568b3d6a87cd873ba4a0214218845ac813b31c
Ali Gholizadeh <71063386+TANKBLOOD@users.noreply.github.com>
2023-09-08 14:56:46 +0200

    Update web.php

--- myshoplv9/routes/web.php
+++ myshoplv9/routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\PageComponents\MainSliderController;
 use App\Http\Controllers\presentable\ProductController;
 use App\Http\Controllers\Relators\CategoryController;
+use App\Models\Presentable\Product;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -37,4 +39,14 @@ Route::get('/admin/category/search', [CategoryController::class, 'search'])->nam
 
 //product section routes
 Route::get('/admin/post/create', [ProductController::class, 'createForm'])->name('admin.product.create');
+Route::get('/admin/product/index', [ProductController::class, 'indexPage'])->name('admin.product.index');
+Route::get('/admin/product/indexData', [ProductController::class, 'index'])->name('admin.product.indexData');
+Route::get('/admin/product/search/{term}', [ProductController::class, 'search'])->name('admin.product.search');
+Route::get('/admin/product/edit/{id}', [ProductController::class, 'editForm'])->name('admin.product.editForm');
+Route::post('/admin/product/update', [ProductController::class, 'update'])->name('admin.product.update');
+Route::delete('/admin/product/delete', [ProductController::class, 'delete'])->name('admin.product.delete');
+
+Route::post('/upload/product/image',[ProductController::class, 'uploadProductImage'])->name('product.upload.image');
+Route::get('/product/getImage/{filename}', [ProductController::class, 'getFile'])->name('product.getfile');
+Route::post('/admin/product/store', [ProductController::class, 'create'])->name('admin.product.store');
 
